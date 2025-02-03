14dfb7753c56cea83e85a470ea622e1ffad18e74
Vishnu-bhat <vishnuramesh1105@gmail.com>
2025-02-03 16:10:50 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    settings: { react: { version: 'detect' } }, // Auto-detect React version
+    plugins: ['react', 'react-hooks', 'react-refresh'], // Plugins should be an array
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
@@ -35,4 +30,4 @@ export default [
       ],
     },
   },
-]
+];
