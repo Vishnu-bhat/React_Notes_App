9ec5d1fa6a5e9f82423b86cc108db5cca1f4e939
Vishnu-bhat <vishnuramesh1105@gmail.com>
2025-02-03 15:47:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    allowedHosts: [
+      "jd6fzn-5174.csb.app",
+      "jd6fzn-5173.csb.app",
+      "jd6fzn-2222.csb.app",
+      "jd6fzn-50000.csb.app",
+    ],
+  },
 })
